dd536be2f685a073887897bbcfa132fb84a89317
david cheng <david@softpower.com.tw>
2016-03-31 16:28:02 +0800

    init

---
+++ src/main/java/com/softpower/Chihuahua2Application.java
@@ -0,0 +1,12 @@
+package com.softpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chihuahua2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chihuahua2Application.class, args);
+	}
+}
